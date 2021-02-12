1802cfff613d45f3e18e8c12e85e60dedb8c24b4
Vachounet <vachounet@live.fr>
2021-02-12 18:47:44 +0100

    sm7250_common: decommonize kernel board flags
    Change-Id: I22e11a1fe50b1313d626ff68531545c728aa4c59

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,20 +28,7 @@ TARGET_BOOTLOADER_BOARD_NAME := lito
 TARGET_BOARD_PLATFORM := lito
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno620
 
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 earlycon=msm_geni_serial,0x888000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket loop.max_part=7 firmware_class.path=/vendor/firmware_mnt/image
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_HEADER_ARCH := arm64
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_SOURCE := kernel/motorola/lito
-
-BOARD_KERNEL_SEPARATED_DTBO := true
-
 TARGET_KERNEL_VERSION := 4.19
-
 
 TARGET_SPECIFIC_HEADER_PATH := $(SM7250_PATH)/include
 
